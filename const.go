54f6f007a82d0e921a9d2ea2c973f86ae3004ee4
XinyueZhao <zhao@eosnewmedia.de>
2015-02-26 13:47:22 +0100

    Add app-id and app-sec to const.

--- const.go
+++ const.go
@@ -2,8 +2,11 @@ package osc_auto_tweet
 
 //Your login account and password of www.oschina.net .
 //You should not check-in this file to repository or only provide empty data.
+//Also oschina application-id and application security should be input here.
 
 const(
 	ACCOUNT = ""
 	PASSWORD = ""
+	APP_ID       = ""
+	APP_SEC      = ""
 )
